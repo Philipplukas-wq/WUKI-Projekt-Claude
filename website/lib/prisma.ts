642d11372ec254cb1211b4678ac1bdfb813b762c
Philipp Lukas <Philipp.lukas@outlook.de>
2026-04-28 12:43:55 +0200

    Fix Prisma 7.x import path

--- website/lib/prisma.ts
+++ website/lib/prisma.ts
@@ -1,10 +1,15 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "@prisma/client/generated";
 
 const globalForPrisma = global as unknown as { prisma?: PrismaClient };
 
 export const prisma =
   globalForPrisma.prisma ||
   new PrismaClient({
+    datasources: {
+      db: {
+        url: process.env.DATABASE_URL,
+      },
+    },
     log: process.env.NODE_ENV === "development" ? ["error"] : [],
   });
 
