f98975f57a28332d25389577a7f8bcbe9ee4bc53
Philipp Lukas <Philipp.lukas@outlook.de>
2026-04-28 13:29:38 +0200

    Prisma 7.x: accelerateUrl hinzugefügt

--- website/lib/prisma.ts
+++ website/lib/prisma.ts
@@ -1,11 +1,12 @@
-import { PrismaClient } from "../prisma/generated/client";
 
-const globalForPrisma = global as unknown as { prisma?: PrismaClient };
+  import { PrismaClient } from "../prisma/generated/client";
 
-export const prisma =
-  globalForPrisma.prisma ||
-  new PrismaClient({
-    log: process.env.NODE_ENV === "development" ? ["error", "warn"] : [],
-  } as any);
+  const globalForPrisma = global as unknown as { prisma?: PrismaClient };
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+  export const prisma =
+    globalForPrisma.prisma ||
+    new PrismaClient({
+      accelerateUrl: process.env.DATABASE_URL,
+    });
+
+  if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
